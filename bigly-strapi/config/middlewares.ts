d2ab1d8a5092a8736510bd82b42fd3eb3de7fffd
Bentley Hensel <bentleyhensel@gmail.com>
2025-10-24 18:40:45 -0400

    feat: Cloudflare R2 Image Storage
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

--- bigly-strapi/config/middlewares.ts
+++ bigly-strapi/config/middlewares.ts
@@ -1,11 +1,56 @@
-export default [
-  'strapi::logger',
+// bigly-strapi/config/middlewares.ts
+export default ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('CF_PUBLIC_ACCESS_URL')
+              ? env('CF_PUBLIC_ACCESS_URL').replace(/^https?:\/\//, '')
+              : '',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('CF_PUBLIC_ACCESS_URL')
+              ? env('CF_PUBLIC_ACCESS_URL').replace(/^https?:\/\//, '')
+              : '',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      // request body limits
+      formLimit: '200mb',
+      jsonLimit: '200mb',
+      textLimit: '200mb',
+
+      // file uploads (multipart/form-data)
+      multipart: true,
+      formidable: {
+        // bytes (200 MB)
+        maxFileSize: 200 * 1024 * 1024,
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
